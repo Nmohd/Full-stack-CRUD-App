89098a030722bba196bac82c043bdc29d1d82ba5
Mohd Najeeb Ullah <49758151+Nmohd@users.noreply.github.com>
2023-07-14 20:03:50 +0530

    Update dbConnect.js

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -1,14 +1,1 @@
-import mongoose from "mongoose";
 
-async function connectDB() {
-    try {
-        await mongoose.connect("mongodb+srv://unknown:12345@sandbox.1nsjpu6.mongodb.net/userdata")
-        console.log("Mongo DB is Connected");
-
-    } catch (error) {
-        console.log("not connected")
-        console.log(error);
-    }
-}
-
-connectDB();
